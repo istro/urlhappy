00140b462177ca307a96a60f129bccd5b4bb7158
Apprentice <apprentice@dbc05.devbootcamp.com>
2012-07-20 12:45:37 -0700

    new rails project, with separate rvm environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urlhappy::Application.config.secret_token = '86a9d2eaa9ba1503c340d64d1b18df86ab1e5e9db351059748009a15cfdb4f0b46e05d0d06895122cb0d732ef449163fe0f2d55a0e4b1d3d1b77a856b1d8b9d0'
